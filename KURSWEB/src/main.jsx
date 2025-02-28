6a7f0051a9d37a9645eba2befb2dd34e0d26f92f
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2025-02-28 18:31:43 +0700

    Update main.jsx

--- KURSWEB/src/main.jsx
+++ KURSWEB/src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CurrencyConverter from '../pages/CurrencyConverter';
-import NotFound from './pages/NotFound';
-import './index.css';
+import NotFound from '../pages/NotFound';
+import '../index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
